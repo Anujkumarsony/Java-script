eeeb5ee73ccfc6565d5c0c45577b4b08027b8af7
Anuj kumar sony <94818996+Anujkumarsony@users.noreply.github.com>
2024-02-27 10:00:05 +0000

    container config done

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId=12342
+let accountName="anuj"
+var accountEmail="anuj@google.com"
+accountPass="00000"
+
+
+//accountId=5555
+accountEmail="ram@google.com"
+accountName="ram"
+accountPass="123"
+
+
+console.table([accountId, accountName, accountEmail, accountPass])
